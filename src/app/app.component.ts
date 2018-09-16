8fa1e03cc2d536dff4d05ea153627001c7424f1a
Joe Eames <joeeames@gmail.com>
2018-09-15 23:45:06 -0600

    added favicon, theme, home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'hm-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'test2';
+  title = 'Healthy Me';
 }
