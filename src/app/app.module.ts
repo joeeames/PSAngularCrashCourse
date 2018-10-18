1457ccb156f76befb3863c633bcc82a62c120440
Joe Eames <joeeames@gmail.com>
2018-10-18 13:58:46 -0600

    added forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -15,6 +16,7 @@ import { NewWeightEntryComponent } from './new-weight-entry/new-weight-entry.com
   imports: [
     CommonModule,
     BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/new-weight-entry/new-weight-entry.component.ts
+++ src/app/new-weight-entry/new-weight-entry.component.ts
@@ -8,14 +8,25 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 export class NewWeightEntryComponent implements OnInit {
   @Input() showBodyFat: boolean;
   @Output() create = new EventEmitter();
+  model;
 
   constructor() { }
 
   ngOnInit() {
+    this.resetForm();
   }
 
   createEntry() {
-    this.create.emit({id:-1,date: new Date('1/15/1996'), weight:110, bodyfat:0.35});
+    let newEntry = Object.assign({}, this.model,
+      {
+        bodyfat: this.model.bodyfat / 100,
+        date: new Date(this.model.date)
+      })
+    this.create.emit(newEntry);
+  }
+
+  resetForm() {
+    this.model = {}
   }
 
 }
